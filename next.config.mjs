4a38dd2df9c22d90dd28d665617d2ac782771cc7
prathamdupare <prathamdupare@proton.me>
2024-02-06 12:42:52 +0530

    reverted to old next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
